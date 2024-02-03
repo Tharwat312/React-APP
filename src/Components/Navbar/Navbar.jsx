76aff66928b8e51fe8c2d65cf63f7e867790aa3d
Tharwat312 <tharwata312@gmail.com>
2024-02-03 22:21:34 +0200

    Added Active Class for Nav Links

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from '../Navbar/Navbar.module.css'
-import { Link } from 'react-router-dom'
+import '../Navbar/Navbar.css'
+import { Link, NavLink } from 'react-router-dom'
 export default function Navbar() {
+    const isClassActive = ({isActive}) => (isActive ? "active" : "");
+    const navlink = `nav-link fw-bold text-uppercase text-white p-2`
     return (
         <nav className={`navbar navbar-expand-lg py-3 fixed-top ${styles.navbar}`}>
             <div className={"container"}>
@@ -12,16 +15,16 @@ export default function Navbar() {
                 <div className={"collapse navbar-collapse"} id="navbarSupportedContent">
                     <ul className={"navbar-nav ms-auto mb-2 mb-lg-0"}>
                         <li className={"nav-item"}>
-                            <Link to={""} className={`nav-link fw-bold text-uppercase text-white ${styles.active} p-2`} href="#">Home</Link>
+                            <NavLink to={""} className={`${navlink} ${isClassActive}`} href="#">Home</NavLink>
                         </li>
                         <li className={"nav-item"}>
-                            <Link to={"about"} className={`nav-link fw-bold text-uppercase text-white p-2`} href="#">about</Link>
+                            <NavLink to={"about"} className={`${navlink} ${isClassActive}`} href="#">about</NavLink>
                         </li>
                         <li className={"nav-item"}>
-                            <Link to={"portfolio"} className={`nav-link fw-bold text-uppercase text-white p-2`} href="#">portfolio</Link>
+                            <NavLink to={"portfolio"} className={`${navlink} ${isClassActive}`} href="#">portfolio</NavLink>
                         </li>
                         <li className={"nav-item"}>
-                            <Link to={"contact"} className={`nav-link fw-bold text-uppercase text-white p-2`} href="#">contact</Link>
+                            <NavLink to={"contact"} className={`${navlink} ${isClassActive}`} href="#">contact</NavLink>
                         </li>
                     </ul>
                 </div>
